e7df27f7209db5857b1fec92f1e3e646ab7844b6
Alexandre Plennevaux <alexandre@pixeline.be>
2015-09-25 17:33:26 +0200

    Added Social buttons in random feed

--- controllers/feed_random.get.php
+++ controllers/feed_random.get.php
@@ -13,13 +13,21 @@
 		if(is_object($item)){
 			$permalink = WWWROOT.'/quote/view/' . $item->quote_id;
 			$filename =$_SERVER['DOCUMENT_ROOT'].'/'.UPLOADS.$item->photo;
+			$share_message = urlencode($item->quote. "\n– ". $item->fullname );
+			$tweet_version = urlencode(truncate($item->fullname. ': '.$item->quote, 90));
 			$photo='';
 			if(!empty($item->photo) && is_file($filename)){
 				$photo = '<div class="avatar" title="'.ucfirst($item->fullname). '" style="margin:1em auto;border-radius:50%;width:200px;height:200px;background-color:white;background-position: center center;background-repeat: no-repeat;background-size:cover;background-image: url('.WWWROOT.'/'.UPLOADS.$item->photo.'); background:url('.WWWROOT.'/'.UPLOADS.$item->photo.') center center / cover no-repeat ; -webkit-box-shadow: inset 0px 0px 45px 5px rgba(18,18,18,0.19);-moz-box-shadow: inset 0px 0px 45px 5px rgba(18,18,18,0.19);box-shadow: inset 0px 0px 45px 5px rgba(18,18,18,0.19);"></div>';
 			}
 			$description = '<div style="text-align:center">';
 			$description .= '<blockquote cite="'.$permalink.'" style="font-family:georgia, serif;font-size:32px;line-height:1.4">' . html_entity_decode($item->quote).'</blockquote>';
-			$description .= $photo.'<strong>'.ucfirst($item->fullname). '</strong></a></div>';
+			$description .= $photo.'<strong>'.ucfirst($item->fullname). '</strong>
+			
+			<p style="text-align:center;line-height:2">
+			<a style="padding:10px;" href="//www.facebook.com/sharer/sharer.php?u='.$permalink.'"><img src="'.WWWROOT.'/ui/img/facebook.png"  width="8" height="13" alt="share this quote on Facebook"></a>
+<a  style="padding:10px;" href="http://twitter.com/share?text='.$tweet_version .'&amp;url='. $permalink .'&amp;hashtags=design_quote"><img  width="15" height="14" src="'.WWWROOT.'/ui/img/twitter.png" alt="share this quote on Twitter"></a>
+</p>
+			</div>';
 
 			$rss1[] = (object)[
 			'title' => truncate($item->quote, 250)
