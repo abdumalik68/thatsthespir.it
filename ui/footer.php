6d0ef804e07b9e821ac66d4bc39bb99528eaaf04
Alexandre Plennevaux <alexandre@pixeline.be>
2015-12-26 11:54:40 +0100

    readme fix

--- ui/footer.php
+++ ui/footer.php
@@ -1,7 +1,7 @@
 <footer class="footer">
 <h1 class="ui-title topline" style="background:white; text-align: left;">Uh?</h1>
 
-	<p>This garden of lights is made for unicorns by a <a href="https://pixeline.be">web designer in Brussels</a>, whose mind needed a place to bathe in all the beautifulities the human mind came up with during our time on Earth. May this place be as enthralling for you as it is for me.<br><img src="ui/img/hand.svg" alt="V for Victory hand sign" class="hand">
+	<p>Made by some <a href="https://pixeline.be" title="Best Brussels web agency">web designer in Brussels</a>.<br><img src="ui/img/hand.svg" alt="V for Victory hand sign" class="hand">
 	</p>
 	<p>
 		<a href="/daily" title="Receive a daily quote from the Spirit in your mailbox">Would you like a Daily quote by email ?</a> - <a href="<?php echo $privacy_url ?>" title="In short: Google Analytics cookies, that's about it.">Privacy policy</a> - <a href="<?php echo $archive_url ?>" title="The Spirit! choses humans as channels. Also known as &lsquo;Authors&rsquo;">All Authors</a>
