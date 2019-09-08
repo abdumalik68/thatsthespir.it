fb029dcd610980277f586aae35955aad9b568918
Alexandre Plennevaux <alexandre@pixeline.be>
2019-09-08 15:38:53 +0200

    cleanup

--- web/app/ui/layout-page.php
+++
@@ -1,9 +0,0 @@
-<?php include 'header.php'; ?>
-	
-<div class="white-background">
-    	<div class="content">
-			<?php echo $this->render($content); ?>
-    	</div>
-</div>
-
-<?php include 'footer.php'; ?>
