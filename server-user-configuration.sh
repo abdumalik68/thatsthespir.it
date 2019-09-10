2851613d88469e2b301c6af5b34059438f761925
Alexandre Plennevaux <alexandre@pixeline.be>
2019-09-10 18:44:10 +0200

    wip

---
+++ server-user-configuration.sh
@@ -0,0 +1,20 @@
+# url: api.thatsthespir.it
+# directory: /home/thatsthespirit/www/api.thatsthespir.it/public/
+
+mkdir -p /home/thatsthespirit/.ssh
+touch /home/thatsthespirit/.ssh/authorized_keys
+useradd -d /home/thatsthespirit thatsthespirit
+usermod -aG sudo thatsthespirit
+chown -R thatsthespirit:thatsthespirit /home/thatsthespirit/
+chown root:root /home/thatsthespirit
+chmod 700 /home/thatsthespirit/.ssh
+chmod 644 /home/thatsthespirit/.ssh/authorized_keys
+
+usermod -aG www-data thatsthespirit
+cd /home/thatsthespirit
+mkdir www
+chown -R www-data:www-data /home/thatsthespirit/www/api.thatsthespir.it/public
+# virtual host
+sudo cp /etc/nginx/sites-available/default /etc/nginx/sites-available/api.thatsthespir.it
+nano /etc/nginx/sites-available/api.thatsthespir.it
+ln -s /etc/nginx/sites-available/api.thatsthespir.it /etc/nginx/sites-enabled/
