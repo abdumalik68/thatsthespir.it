9ef240680c26c8e6f18e229d56a94f48a14a7996
Alexandre Plennevaux <alexandre@pixeline.be>
2019-09-11 23:57:58 +0200

    wip

--- web/app/api/Quote.php
+++ web/app/api/Quote.php
@@ -4,6 +4,18 @@ class Quote
 {
     protected $selected_fields = 'SELECT q.id, q.id as quote_id, q.quote, q.source, q.status, q.creation_date, a.id as author_id, a.fullname,a.slug,a.photo,a.total,a.gender, tags_id, (SELECT group_concat(name) from tags as t where t.id=q.tags_id) as tags, (SELECT group_concat(fullname) FROM `favourites` LEFT JOIN users as u on u.email=favourites.user_email WHERE quote_id=q.id) as likers ';
     public $quote;
+    private $validation_rules = array(
+        'quote'         => 'required|alpha_numeric|min_len,6',
+        'author_id'     => 'required|integer',
+        'source'        => 'required|valid_url',
+        'tags_id'       => 'required|integer',
+        'submitted_by'  => 'required|integer'
+    );
+    private $filter_rules = array(
+        'quote'         => 'trim|sanitize_string',
+        'source'        => 'trim|sanitize_url',
+        'slug'          => 'slug',
+    );
 
     function __construct()
     {
@@ -27,9 +39,10 @@ class Quote
         send_json($this->quote->cast());
     }
     function post($f3, $params)
-    { }
-    function put($f3, $params)
     {
+        /**
+         * Create Element
+         */
         if (!$f3->exists('POST')) {
             $f3->error(403, 'No data sent.');
         }
@@ -39,18 +52,43 @@ class Quote
         // sanitize
         $f3->POST = $gump->sanitize($f3->get('POST'));
         // validate
-        $gump->validation_rules(array(
-            'quote'    => 'required|alpha_numeric|min_len,6',
-            'author_id'    => 'required|integer',
-            'source'       => 'required|valid_url',
-            'tags_id'      => 'required|integer',
-            'submitted_by' => 'required|integer'
-        ));
-        $gump->filter_rules(array(
-            'quote' => 'trim|sanitize_string',
-            'source'    => 'trim|sanitize_url',
-            'slug'   => 'slug',
-        ));
+        $gump->validation_rules($this->validation_rules);
+        $gump->filter_rules($this->filter_rules);
+
+        $validated_data = $gump->run($f3->POST);
+        // execute
+        if ($validated_data === false) {
+            echo $gump->get_readable_errors(true);
+        } else {
+            pr($validated_data);
+            // validation successful
+            $this->quote = $f3->copyFrom($validated_data);
+            if (!$this->quote->save()) {
+                $f3->error(410, 'Quote could not be saved.');
+            } else {
+                send_json(array('code' => 200, 'message' => 'Quote successfully created.'));
+            }
+        }
+        // return
+    }
+    function put($f3, $params)
+    {
+        /**
+         * Update Element
+         */
+        if (!$f3->exists('POST')) {
+            $f3->error(403, 'No data sent.');
+        }
+        $gump = new GUMP();
+        // Prepare for slug creation, based on the quote itself
+        $f3->set('POST.slug', $f3->get('POST.quote'));
+        // sanitize
+        $f3->POST = $gump->sanitize($f3->get('POST'));
+        // validate
+        $validation_rules = $this->validation_rules;
+        $validation_rules['id'] = 'required|numeric';
+        $gump->validation_rules($validation_rules);
+        $gump->filter_rules($this->filter_rules);
 
         $validated_data = $gump->run($f3->POST);
         // execute
