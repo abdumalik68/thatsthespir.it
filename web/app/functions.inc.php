53099f882eac719e717c2a85b136e974a95eed99
Alexandre Plennevaux <alexandre@pixeline.be>
2019-09-17 16:11:20 +0200

    improve slug

--- web/app/functions.inc.php
+++ web/app/functions.inc.php
@@ -29,7 +29,7 @@ function create_slug($str, $delimiter = '-')
 	/**
 	 * source: https://stackoverflow.com/questions/40641973/php-to-convert-string-to-slug
 	 */
-
+	$str = strip_tags(trim($str));
 	$slug = strtolower(trim(preg_replace('/[\s-]+/', $delimiter, preg_replace('/[^A-Za-z0-9-]+/', $delimiter, preg_replace('/[&]/', 'and', preg_replace('/[\']/', '', iconv('UTF-8', 'ASCII//TRANSLIT', $str))))), $delimiter));
 	return $slug;
 }
